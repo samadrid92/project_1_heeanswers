d75e8e84a2baff60056676de53f27b3e5515fb5f
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2016-04-07 11:59:23 -0700

    set up basic html & able to render hard data to page

---
+++ public/scripts/app.js
@@ -0,0 +1,40 @@
+console.log("sanity check");
+var sampleAlbums = [];
+sampleAlbums.push({
+             artistName: 'Ladyhawke',
+             name: 'Ladyhawke',
+             releaseDate: '2008, November 18',
+             genres: [ 'new wave', 'indie rock', 'synth pop' ]
+           });
+sampleAlbums.push({
+             artistName: 'The Knife',
+             name: 'Silent Shout',
+             releaseDate: '2006, February 17',
+             genres: [ 'synth pop', 'electronica', 'experimental' ]
+           });
+sampleAlbums.push({
+             artistName: 'Juno Reactor',
+             name: 'Shango',
+             releaseDate: '2000, October 9',
+             genres: [ 'electronic', 'goa trance', 'tribal house' ]
+           });
+sampleAlbums.push({
+             artistName: 'Philip Wesley',
+             name: 'Dark Night of the Soul',
+             releaseDate: '2008, September 12',
+             genres: [ 'piano' ]
+           });
+
+$(document).ready(function() {
+  console.log("app.js works");
+  renderScripture(sampleAlbums[0]);
+});
+
+function renderScripture(data) {
+  console.log('rendering scripture:', data);
+  var scriptureHtml = $('#scripture-template').html();
+  var scriptureTemplate = Handlebars.compile(scriptureHtml);
+  var html = scriptureTemplate({scripture: data});
+  console.log(html);
+  $('#scripture-target').append(html);
+ }
---
+++ server.js
@@ -0,0 +1,48 @@
+// SERVER-SIDE JAVASCRIPT
+
+//require express in our app
+var express = require('express');
+// generate a new express app and call it 'app'
+var app = express();
+
+// serve static files from public folder
+app.use(express.static(__dirname + '/public'));
+
+// We'll serve jQuery and bootstrap from a local bower cache avoiding CDNs
+// We're placing these under /vendor to differentiate them from our own assets
+app.use('/vendor', express.static(__dirname + '/bower_components'));
+
+var controllers = require('./controllers'),
+    db = require('./models'),
+    bodyParser = require('body-parser');
+
+//body parserconfig to accept our datatypes
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+/**********
+ * ROUTES *
+ **********/
+
+/*
+ * HTML Endpoints
+ */
+
+app.get('/', function homepage (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+/*
+ * JSON API Endpoints
+ */
+
+
+/**********
+ * SERVER *
+ **********/
+
+// listen on port 3000
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Express server is running on http://localhost:3000/');
+});
