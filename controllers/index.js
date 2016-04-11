329d8501a8134b67c88f4be066b6889b4ac854cc
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2016-04-11 16:35:36 -0700

    started to edit controller for 2nd model

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,2 @@
 module.exports.api = require('./apiController');
 module.exports.scriptures = require('./scriptureController.js');
-module.exports.scriptureHelp = require('./scriptureHelpController');
