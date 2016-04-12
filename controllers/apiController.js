b7e660764953fac0c0a89de4ad00e7573a1c3fd1
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2016-04-11 22:55:07 -0700

    started to add endpoints

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,10 +1,15 @@
 function index(req, res) {
   res.json({
-    message: "Welcome to tunely!",
-    documentation_url: "https://github.com/tgaff/tunely/api.md",
-    base_url: "http://tunely.herokuapp.com",
+    message: "HeeAnswers: When google dosen't have the answer, HeeAnswers will!",
+    documentation_url: "https://github.com/samadrid92/project_1_heeanswers",
+    base_url: "https://mighty-garden-14500.herokuapp.com/",
     endpoints: [
-      {method: "GET", path: "/api", description: "Describes available endpoints"}
+      {method: "GET", path: "/api", description: "Describes available endpoints"},
+      {method: "GET", path: "/api/scriptures", description: ""},
+      {method: "GET", path: "/api/scriptures/:emotion", description: ""},
+      {method: "POST", path: "/api/scriptures", description: ""},
+      {method: "DELETE", path: "/api/scriptures/:emotion_id", description: ""},
+      {method: "PUT", path: "/api/scriptures/:emotion", description: ""}
     ]
   });
 }
